c0bc8be0851ee9a3b44495f3d9ae43540abd58f3
Juutis <>
2024-02-25 13:51:21 +0200

    lalal

---
+++ Clicker/Assets/Scripts/GFX/UpgradeGraphics.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeGraphics : MonoBehaviour
+{
+
+    public static UpgradeGraphics Main;
+
+    [SerializeField]
+    private List<GameObject> balloons;
+
+    [SerializeField]
+    private List<GameObject> rockets;
+
+    [SerializeField]
+    private List<GameObject> crystals;
+
+    [SerializeField]
+    private List<GameObject> cauldrons;
+
+    [SerializeField]
+    private List<GameObject> catchers;
+
+    [SerializeField]
+    private List<GameObject> bubbles;
+
+    [SerializeField]
+    private List<GameObject> devices;
+
+    void Awake() {
+        Main = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetBalloonLevel(int level) {
+        activateGameObjects(balloons, level);
+    }
+
+    public void SetRocketLevel(int level) {
+        activateGameObjects(rockets, level);
+    }
+
+    public void SetCrystalLevel(int level) {
+        activateGameObjects(crystals, level);
+    }
+
+    public void SetCauldronLevel(int level) {
+        activateGameObjects(cauldrons, level);
+    }
+
+    public void SetCatcherLevel(int level) {
+        activateGameObjects(catchers, level);
+    }
+
+    public void SetBubbleLevel(int level) {
+        activateGameObjects(bubbles, level);
+    }
+
+    public void SetDeviceLevel(int level) {
+        activateGameObjects(devices, level);
+    }
+
+    private void activateGameObjects(List<GameObject> objects, int count) {
+        for (var i = 0; i < objects.Count; i++) {
+            objects[i].SetActive(i < count);
+        }
+    }
+}
